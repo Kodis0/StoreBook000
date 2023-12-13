152002f0a6051d77765303449e6d8609e13e7e42
Kodis0 <ekimta110@gmail.com>
2023-12-14 00:05:45 +0300

    Обновление данных

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Store
+{
+    public class Book
+    {
+        public string Title { get; }
+
+        public int Id { get; }
+
+        public string Author { get; }
+
+        public int Price { get; }
+
+        public string Describe { get; }
+
+        public string Publishing { get; }
+
+        public int Year { get; }
+
+        public int Grade { get; }
+
+        public Book(int id, int price, string author, string title,
+            string describe, string publishing, int year, int grade)
+        {
+            Title = title;
+            Id = id;
+            Author = author;
+            Price = price;
+            Describe = describe;
+            Publishing = publishing;
+            Year = year;
+            Grade = grade;
+        }
+    }
+}
